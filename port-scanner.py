3c979cefe45e37a643254f612b531e6047a5cb5d
AlfieMcPhee <mcphee.dev@proton.me>
2026-07-08 13:30:15 +0100

    Added functions to aid further code

--- port-scanner.py
+++ port-scanner.py
@@ -5,3 +5,21 @@ from concurrent.futures import ThreadPoolExecutor, as_completed
 
 # Importing Libraries Above
 
+def resolve_host(host):
+    # Converts a hostname into an IP address
+    try:
+        ip = socket.gethostbyname(host)
+        return ip
+    except socket.gaierror:
+        print(f"[ERROR] Could not resolve host: {host}")
+        raise SystemExit(1)
+
+
+
+def  get_service_name(port):
+    #Looks up a service name per port
+    try:
+        return socket.getservbyport(port)
+    except OSError:
+        return "unknown"
+
